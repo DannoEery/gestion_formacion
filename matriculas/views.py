3e58804f99f22dde2261bbd1c93dd56073566a5b
Daniel <danielgilbarrientos@gmail.com>
2026-06-18 13:50:53 +0200

    Modificado templates/base.html

--- matriculas/views.py
+++ matriculas/views.py
@@ -9,26 +9,71 @@ from .models import Matricula
 # =========================================================================
 @login_required
 def matricularse(request, curso_id):
-    # Busca el curso por su ID. Si no existe o no está activo, devuelve un error 404 de inmediato
-    curso = get_object_or_404(Curso, pk=curso_id, activo=True)
 
-    # Intenta obtener la matrícula. Si no existe, la crea en la base de datos automáticamente.
-    # 'matricula' contiene el objeto y 'creada' es un booleano (True si se creó, False si ya existía)
-    matricula, creada = Matricula.objects.get_or_create(
-        alumno=request.user,  # Asigna al usuario que tiene la sesión iniciada
-        curso=curso,          # Asigna el curso encontrado arriba
+    curso = get_object_or_404(
+        Curso,
+        id=curso_id,
+        activo=True
     )
 
-    # Envía una alerta de éxito si el registro es nuevo en la base de datos
+    # ==================================
+    # 16. SOLO ALUMNOS
+    # ==================================
+
+    if request.user.tipo != "alumno":
+
+        messages.error(
+            request,
+            "Sólo los alumnos pueden matricularse."
+        )
+
+        return redirect(
+            'detalle_curso',
+            curso.id
+        )
+
+    # ==================================
+    # 14. CONTROL DE PLAZAS
+    # ==================================
+
+    inscritos = curso.matriculas.count()
+
+    if inscritos >= curso.plazas:
+
+        messages.error(
+            request,
+            "No quedan plazas."
+        )
+
+        return redirect(
+            'detalle_curso',
+            curso.id
+        )
+
+    # ==================================
+    # EVITAR DUPLICADOS
+    # ==================================
+
+    matricula, creada = Matricula.objects.get_or_create(
+        alumno=request.user,
+        curso=curso
+    )
+
     if creada:
-        messages.success(request, "Matrícula realizada correctamente.")
-    # Envía una alerta de advertencia si el usuario ya estaba inscrito previamente
+        messages.success(
+            request,
+            "Matrícula realizada correctamente."
+        )
     else:
-        messages.warning(request, "Ya estás matriculado en este curso.")
+        messages.warning(
+            request,
+            "Ya estás matriculado en este curso."
+        )
 
-    # Redirige al usuario de vuelta a la página de detalles de ese mismo curso
-    return redirect("detalle_curso", curso.id)
-
+    return redirect(
+        'detalle_curso',
+        curso.id
+    )
 
 # =========================================================================
 # VISTA: MIS CURSOS
