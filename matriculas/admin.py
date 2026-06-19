de0ef8e0af4bbfd81151b45087c4c31a91c2cede
Daniel <danielgilbarrientos@gmail.com>
2026-06-19 10:52:09 +0200

    Añadido

--- matriculas/admin.py
+++ matriculas/admin.py
@@ -32,8 +32,16 @@ class MatriculaAdmin(admin.ModelAdmin):
     # ==========================
 
     date_hierarchy = 'fecha_matricula'
-    
+
     # Orden predeterminado en el que se mostrarán los registros de la lista
     ordering = (
         '-fecha_matricula', # Ordena de forma descendente (el '-' significa de la más reciente a la más antigua)
+    )
+
+    # ==========================
+    # CAMPOS SOLO LECTURA
+    # ==========================
+
+    readonly_fields = (
+        'fecha_matricula',
     )
