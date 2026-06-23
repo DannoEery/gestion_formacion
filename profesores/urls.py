61557a96f2f1b3154c856b59d121e9d6322fc4f9
Daniel <danielgilbarrientos@gmail.com>
2026-06-23 10:00:23 +0200

    Creado profesores/vies.py

---
+++ profesores/urls.py
@@ -0,0 +1,17 @@
+# Importa la función path para definir rutas de URL individuales en Django
+from django.urls import path
+# Importa la vista del panel de control que creamos para el profesor
+from .views import dashboard_profesor
+
+# Lista que almacena todas las rutas de URL accesibles para este módulo
+urlpatterns = [
+    # Define la ruta web que conecta la URL con la vista del profesor
+    path(
+        # El subdominio o texto que el usuario verá en el navegador (ej: ://tusitio.com)
+        'dashboard/',
+        # La función de la vista que Django ejecutará cuando se visite esta URL
+        dashboard_profesor,
+        # El nombre único de la ruta para poder referenciarla en plantillas o redirecciones
+        name='dashboard_profesor'
+    ),
+]
